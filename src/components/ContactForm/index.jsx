31a2308eb8e59e3c422acc21698d1851b9517411
Joao Vitor <viictorsouza97@gmail.com>
2022-07-17 21:40:07 -0300

    feat => add ContactForm

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Input } from '../Input'
+import { Select } from '../Select'
+import { Button } from '../Button'
+import { Container, ButtonContainer } from './style'
+import { FormGroup } from '../FormGroup/index'
+import PropTypes from 'prop-types'
+
+export const ContactForm = ({ buttonLabel }) => {
+  return (
+    <Container>
+      <FormGroup>
+        <Input type="text" placeholder="Nome" />
+      </FormGroup>
+
+      <FormGroup>
+        <Input type="email" placeholder="Email" />
+      </FormGroup>
+
+      <FormGroup>
+        <Input type="phone" placeholder="Telefone" />
+      </FormGroup>
+
+      <FormGroup>
+        <Select>
+          <option value="123">Instagram</option>
+          <option value="123">Twitter</option>
+          <option value="123">Tik Tok</option>
+        </Select>
+      </FormGroup>
+
+      <ButtonContainer>
+        <Button>{buttonLabel}</Button>
+      </ButtonContainer>
+    </Container>
+  )
+}
+
+ContactForm.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
